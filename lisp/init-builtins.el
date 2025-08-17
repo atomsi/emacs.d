8f672236411554052668eb4950cf8ed8502e81fb
atomsi <si.jiangping@gmail.com>
2025-08-17 15:21:54 +0800

    Update init-builtins.el

--- lisp/init-builtins.el
+++ lisp/init-builtins.el
@@ -3,206 +3,218 @@
 ;;; Code:
 
 ;; os & coding settings
-(when (and cabins-os-win
-	   (boundp 'w32-get-true-file-attributes))
+(when (and (boundp 'cabins-os-win) cabins-os-win
+           (boundp 'w32-get-true-file-attributes))
   (setq w32-get-true-file-attributes nil
-	w32-pipe-read-delay 0
-	w32-pipe-buffer-size (* 64 1024)))
+        w32-pipe-read-delay 0
+        w32-pipe-buffer-size (* 64 1024)))
 
-(when cabins-os-mac
-  (setq mac-command-modifier 'meta
-	mac-option-modifier 'super
-	ns-use-native-fullscreen t))
+(when (and (boundp 'cabins-os-mac) cabins-os-mac)
+  (when (boundp 'mac-command-modifier)
+    (setq mac-command-modifier 'meta))
+  (when (boundp 'mac-option-modifier)
+    (setq mac-option-modifier 'super))
+  (when (boundp 'ns-use-native-fullscreen)
+    (setq ns-use-native-fullscreen t)))
 
-;; solve the Chinese paste issue
-;; let Emacs auto-guess the selection coding according to the Windows/system settings
+;; solve the Chinese paste issue / selection coding
 (prefer-coding-system 'utf-8)
-(unless cabins-os-win
-  (set-selection-coding-system 'utf-8))
+(unless (and (boundp 'cabins-os-win) cabins-os-win)
+  (when (fboundp 'set-selection-coding-system)
+    (set-selection-coding-system 'utf-8)))
 
-;; core package settings
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+;; NOTE: package-archives removed per request
 
-;; make use-package default behavior better
-;; with `use-package-always-ensure' you won't need ":ensure t" all the time
-;; with `use-package-always-defer' you won't need ":defer t" all the time
-(setq use-package-enable-imenu-support t
-      use-package-expand-minimally t)
+;; make use-package default behavior better (if use-package is present)
+(when (featurep 'use-package)
+  (setq use-package-enable-imenu-support t
+        use-package-expand-minimally t))
 
-;; Emacs builtin packages
+;; Emacs builtin packages / defaults
 (setq-default auto-window-vscroll nil
-	      default-directory "~"
-	      default-text-properties '(line-spacing 0.2 line-height 1.2) ;default line height
-	      frame-title-format "%b"
-	      help-window-select t
-	      initial-major-mode 'fundamental-mode
-	      inhibit-startup-screen t ; disable the startup screen splash
-	      kill-whole-line t
-	      mode-line-compact t
-	      make-backup-files nil	; disable backup file
-	      read-process-output-max (* 4 1024 1024)
-	      require-final-newline t
-	      scroll-conservatively 1000
-	      show-trailing-whitespace t
-	      system-time-locale "C"
-	      use-short-answers t)
+              default-directory "~"
+              default-text-properties '(line-spacing 0.2 line-height 1.2) ; default line height
+              frame-title-format "%b"
+              help-window-select t
+              initial-major-mode 'fundamental-mode
+              inhibit-startup-screen t ; disable the startup screen splash
+              kill-whole-line t
+              make-backup-files nil	; disable backup file
+              read-process-output-max (* 4 1024 1024)
+              require-final-newline t
+              scroll-conservatively 1000
+              show-trailing-whitespace t
+              system-time-locale "C"
+              use-short-answers t)
 
 ;; isearch
-(use-package isearch
-  :config
+(when (featurep 'isearch)
   (setq-default isearch-allow-motion t
-		isearch-lazy-count t))
+                isearch-lazy-count t))
 
 ;; auto revert
-;; `global-auto-revert-mode' is provided by autorevert.el (builtin)
-(use-package autorevert
-  :hook (after-init . global-auto-revert-mode))
+(when (featurep 'autorevert)
+  (add-hook 'after-init-hook #'global-auto-revert-mode))
 
 ;; auto save to the visited file (provided by `files.el')
-(use-package files
-  :hook (after-init . auto-save-visited-mode))
+(when (featurep 'files)
+  (add-hook 'after-init-hook #'auto-save-visited-mode))
 
 ;; Delete Behavior
-;; `delete-selection-mode' is provided by delsel.el (builtin)
-(use-package delsel
-  :hook (after-init . delete-selection-mode))
+(when (featurep 'delsel)
+  (add-hook 'after-init-hook #'delete-selection-mode))
 
-;; visual-line-mode
-(use-package simple
-  :hook (prog-mode . visual-line-mode))
+;; visual-line-mode for programming buffers
+(when (featurep 'simple)
+  (add-hook 'prog-mode-hook #'visual-line-mode))
 
-;; pixel-scroll-precise-mode
-(use-package pixel-scroll
-  :hook (after-init . pixel-scroll-precision-mode))
+;; pixel-scroll-precision-mode (if available)
+(when (fboundp 'pixel-scroll-precision-mode)
+  (add-hook 'after-init-hook #'pixel-scroll-precision-mode))
 
-;; fido-mode
-;; `fido-mode' is provided by icomplete.el
-(use-package icomplete
-  :hook (after-init . fido-mode)
-  :config (setq completions-detailed t))
+;; fido-mode (icomplete) if available
+(when (featurep 'icomplete)
+  (when (fboundp 'fido-mode)
+    (add-hook 'after-init-hook #'fido-mode))
+  (setq completions-detailed t))
 
-;; Highlight Current Line
-(use-package hl-line
-  :when (display-graphic-p)
-  :hook (prog-mode . hl-line-mode))
+;; Highlight Current Line in GUI frames
+(when (and (display-graphic-p) (featurep 'hl-line))
+  (add-hook 'prog-mode-hook #'hl-line-mode))
 
-;; ibuffer
-(defalias 'list-buffers 'ibuffer)
+;; ibuffer alias
+(when (featurep 'ibuffer)
+  (defalias 'list-buffers 'ibuffer))
 
-;; Org Mode
-(use-package org
-  :hook (org-mode . org-num-mode)
-  :config
+;; Org Mode: enable org-num-mode only if present
+(when (featurep 'org)
+  (add-hook 'org-mode-hook (lambda ()
+                             (when (fboundp 'org-num-mode)
+                               (org-num-mode))))
   (setq org-hide-leading-stars t
-	org-hide-emphasis-markers t
-	org-startup-indented t))
+        org-hide-emphasis-markers t
+        org-startup-indented t))
 
-;; Pulse the cursor line
-(dolist (cmd '(recenter-top-bottom other-window))
-  (advice-add cmd :after (lambda (&rest _) (pulse-momentary-highlight-one-line))))
+;; Pulse the cursor line if available
+(when (fboundp 'pulse-momentary-highlight-one-line)
+  (dolist (cmd '(recenter-top-bottom other-window))
+    (advice-add cmd :after (lambda (&rest _) (pulse-momentary-highlight-one-line)))))
 
 ;; Show Paren Mode
-(use-package paren
-  :config
+(when (featurep 'paren)
   (setq show-paren-when-point-in-periphery t
-	show-paren-when-point-inside-paren t
-	show-paren-style 'mixed))
+        show-paren-when-point-inside-paren t
+        show-paren-style 'mixed))
 
 ;; Recentf
-(use-package recentf
-  :hook (after-init . recentf-mode)
-  ;; recentf-open since v29.1, recentf-open-files since v22
-  :bind (("C-c r" . #'recentf-open)))
+(when (featurep 'recentf)
+  (add-hook 'after-init-hook #'recentf-mode)
+  (global-set-key
+   (kbd "C-c r")
+   (lambda ()
+     (interactive)
+     (cond
+      ((fboundp 'recentf-open)
+       (call-interactively 'recentf-open))
+      ((fboundp 'recentf-open-files)
+       (call-interactively 'recentf-open-files))
+      (t
+       (user-error "recentf-open / recentf-open-files not available"))))))
 
-;; windmove.el, use  <SHIFT - arrow key> to switch buffers
-(use-package windmove
-  :config (windmove-default-keybindings))
+;; windmove
+(when (featurep 'windmove)
+  (windmove-default-keybindings))
 
-;; Settings for which-key - suggest next key, Builtin package now
-(use-package which-key
-  :hook (after-init . which-key-mode))
+;; which-key: only enable if available (not assuming installed)
+(when (require 'which-key nil 'noerror)
+  (add-hook 'after-init-hook #'which-key-mode))
 
-;; programming language hooks
-(add-hook 'prog-mode-hook (lambda () (setq-local column-number-mode t)))
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(add-hook 'prog-mode-hook 'electric-pair-mode)
-(add-hook 'prog-mode-hook 'hs-minor-mode)
-(add-hook 'prog-mode-hook 'prettify-symbols-mode)
-(add-hook 'prog-mode-hook 'which-function-mode)
+;; Consolidated prog-mode hook to enable common programming helpers
+(add-hook 'prog-mode-hook
+          (lambda ()
+            ;; show column number in mode line for this buffer
+            (when (boundp 'column-number-mode)
+              (setq-local column-number-mode t))
+            ;; modes that may or may not be present in the build
+            (when (fboundp 'display-line-numbers-mode)
+              (display-line-numbers-mode))
+            (when (fboundp 'electric-pair-mode)
+              (electric-pair-mode))
+            (when (fboundp 'hs-minor-mode)
+              (hs-minor-mode))
+            (when (fboundp 'prettify-symbols-mode)
+              (prettify-symbols-mode))
+            (when (fboundp 'which-function-mode)
+              (which-function-mode))))
 
 ;; Flymake
-(use-package flymake
-  :hook (prog-mode . flymake-mode)
-  :bind (("M-n" . #'flymake-goto-next-error)
-	 ("M-p" . #'flymake-goto-prev-error)))
+(when (featurep 'flymake)
+  (add-hook 'prog-mode-hook #'flymake-mode)
+  (global-set-key (kbd "M-n") #'flymake-goto-next-error)
+  (global-set-key (kbd "M-p") #'flymake-goto-prev-error))
 
-
-;; Language Server (eglot - builtin since v29)
-(use-package eglot
-  :bind ("C-c e f" . eglot-format)
-  :init
-  (advice-add 'eglot-code-action-organize-imports :before #'eglot-format-buffer)
-  (add-hook 'eglot-managed-mode-hook (lambda () (add-hook 'before-save-hook #'eglot-format-buffer)))
+;; Eglot (language server client)
+(when (featurep 'eglot)
+  (global-set-key (kbd "C-c e f") #'eglot-format)
+  (when (fboundp 'eglot-code-action-organize-imports)
+    (advice-add 'eglot-code-action-organize-imports :before #'eglot-format-buffer))
+  (add-hook 'eglot-managed-mode-hook
+            (lambda () (when (fboundp 'eglot-format-buffer)
+                         (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
   (add-hook 'prog-mode-hook
-	    (lambda () (unless (member major-mode '(emacs-lisp-mode))
-			 (eglot-ensure)))))
+            (lambda ()
+              (unless (eq major-mode 'emacs-lisp-mode)
+                (when (fboundp 'eglot-ensure)
+                  (eglot-ensure))))))
 
-(use-package treesit
-  :when (and (fboundp 'treesit-available-p) (treesit-available-p))
-  :mode (("\\(?:Dockerfile\\(?:\\..*\\)?\\|\\.[Dd]ockerfile\\)\\'" . dockerfile-ts-mode)
-	 ("\\.go\\'" . go-ts-mode)
-	 ("/go\\.mod\\'" . go-mod-ts-mode)
-	 ("\\.rs\\'" . rust-ts-mode)
-	 ("\\.ts\\'" . typescript-ts-mode)
-	 ("\\.y[a]?ml\\'" . yaml-ts-mode))
-  :config (setq treesit-font-lock-level 4)
-  :init
-  (setq major-mode-remap-alist
-	'((sh-mode         . bash-ts-mode)
-	  (c-mode          . c-ts-mode)
-	  (c++-mode        . c++-ts-mode)
-	  (c-or-c++-mode   . c-or-c++-ts-mode)
-	  (css-mode        . css-ts-mode)
-	  (js-mode         . js-ts-mode)
-	  (java-mode       . java-ts-mode)
-	  (js-json-mode    . json-ts-mode)
-	  (makefile-mode   . cmake-ts-mode)
-	  (python-mode     . python-ts-mode)
-	  (ruby-mode       . ruby-ts-mode)
-	  (conf-toml-mode  . toml-ts-mode)))
-  (setq treesit-language-source-alist
-	'((bash       . ("https://github.com/tree-sitter/tree-sitter-bash"))
-	  (c          . ("https://github.com/tree-sitter/tree-sitter-c"))
-	  (cpp        . ("https://github.com/tree-sitter/tree-sitter-cpp"))
-	  (css        . ("https://github.com/tree-sitter/tree-sitter-css"))
-	  (cmake      . ("https://github.com/uyha/tree-sitter-cmake"))
-	  (csharp     . ("https://github.com/tree-sitter/tree-sitter-c-sharp.git"))
-	  (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
-	  (elisp      . ("https://github.com/Wilfred/tree-sitter-elisp"))
-	  (go         . ("https://github.com/tree-sitter/tree-sitter-go"))
-	  (gomod      . ("https://github.com/camdencheek/tree-sitter-go-mod.git"))
-	  (html       . ("https://github.com/tree-sitter/tree-sitter-html"))
-	  (java       . ("https://github.com/tree-sitter/tree-sitter-java.git"))
-	  (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
-	  (json       . ("https://github.com/tree-sitter/tree-sitter-json"))
-	  (lua        . ("https://github.com/Azganoth/tree-sitter-lua"))
-	  (make       . ("https://github.com/alemuller/tree-sitter-make"))
-	  (markdown   . ("https://github.com/MDeiml/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
-	  (ocaml      . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
-	  (org        . ("https://github.com/milisims/tree-sitter-org"))
-	  (python     . ("https://github.com/tree-sitter/tree-sitter-python"))
-	  (php        . ("https://github.com/tree-sitter/tree-sitter-php"))
-	  (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-	  (tsx        . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
-	  (ruby       . ("https://github.com/tree-sitter/tree-sitter-ruby"))
-	  (rust       . ("https://github.com/tree-sitter/tree-sitter-rust"))
-	  (sql        . ("https://github.com/m-novikov/tree-sitter-sql"))
-	  (vue        . ("https://github.com/merico-dev/tree-sitter-vue"))
-	  (yaml       . ("https://github.com/ikatyang/tree-sitter-yaml"))
-	  (toml       . ("https://github.com/tree-sitter/tree-sitter-toml"))
-	  (zig        . ("https://github.com/GrayJack/tree-sitter-zig")))))
+;; treesit (if available)
+(when (and (fboundp 'treesit-available-p) (treesit-available-p))
+  (when (require 'treesit nil 'noerror)
+    (setq treesit-font-lock-level 4)
+    (setq major-mode-remap-alist
+          '((sh-mode         . bash-ts-mode)
+            (c-mode          . c-ts-mode)
+            (c++-mode        . c++-ts-mode)
+            (c-or-c++-mode   . c-or-c++-ts-mode)
+            (css-mode        . css-ts-mode)
+            (js-mode         . js-ts-mode)
+            (java-mode       . java-ts-mode)
+            (js-json-mode    . json-ts-mode)
+            (makefile-mode   . cmake-ts-mode)
+            (python-mode     . python-ts-mode)
+            (ruby-mode       . ruby-ts-mode)
+            (conf-toml-mode  . toml-ts-mode)))
+    (setq treesit-language-source-alist
+          '((bash       . ("https://github.com/tree-sitter/tree-sitter-bash"))
+            (c          . ("https://github.com/tree-sitter/tree-sitter-c"))
+            (cpp        . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+            (css        . ("https://github.com/tree-sitter/tree-sitter-css"))
+            (cmake      . ("https://github.com/uyha/tree-sitter-cmake"))
+            (csharp     . ("https://github.com/tree-sitter/tree-sitter-c-sharp.git"))
+            (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
+            (elisp      . ("https://github.com/Wilfred/tree-sitter-elisp"))
+            (go         . ("https://github.com/tree-sitter/tree-sitter-go"))
+            (gomod      . ("https://github.com/camdencheek/tree-sitter-go-mod.git"))
+            (html       . ("https://github.com/tree-sitter/tree-sitter-html"))
+            (java       . ("https://github.com/tree-sitter/tree-sitter-java.git"))
+            (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+            (json       . ("https://github.com/tree-sitter/tree-sitter-json"))
+            (lua        . ("https://github.com/Azganoth/tree-sitter-lua"))
+            (make       . ("https://github.com/alemuller/tree-sitter-make"))
+            (markdown   . ("https://github.com/MDeiml/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
+            (ocaml      . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
+            (org        . ("https://github.com/milisims/tree-sitter-org"))
+            (python     . ("https://github.com/tree-sitter/tree-sitter-python"))
+            (php        . ("https://github.com/tree-sitter/tree-sitter-php"))
+            (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
+            (tsx        . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
+            (ruby       . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+            (rust       . ("https://github.com/tree-sitter/tree-sitter-rust"))
+            (sql        . ("https://github.com/m-novikov/tree-sitter-sql"))
+            (vue        . ("https://github.com/merico-dev/tree-sitter-vue"))
+            (yaml       . ("https://github.com/ikatyang/tree-sitter-yaml"))
+            (toml       . ("https://github.com/tree-sitter/tree-sitter-toml"))
+            (zig        . ("https://github.com/GrayJack/tree-sitter-zig"))))))
 
 (provide 'init-builtins)
 
